2ebc8aa76cc3a32035b300be657f482a2130db3b
Matt <oystermenator@gmail.com>
2021-10-15 22:14:54 +0100

    Adding books via DOM method

--- scripts.js
+++ scripts.js
@@ -1,13 +1,40 @@
-let myLibrary = ["To Kill a Mockingbird", "Stoner", "1984"];
+let myLibrary = [
+  "To Kill a Mockingbird",
+  "Stoner",
+  "1984",
+  "In Search of Lost Time",
+  "Ulysses",
+  "Don Quixote",
+  "One Hundred Years of Solitude",
+  "The Great Gatsby",
+  "Moby Dick",
+  "War and Peace",
+];
 
-function Book() {
-  // the constructor...
-}
+  function book() {
+    // the constructor...
+  }
 
 function addBookToLibrary() {
-    const input = prompt("Enter the book name");
-    myLibrary.push(input);
+  const input = prompt("Enter the book name");
+  myLibrary.push(input);
 }
 
-// addBookToLibrary();
-console.table(myLibrary);
+function generateCard(bookName) {
+  let div = document.createElement('div');
+  div.className = 'book';
+  div.innerHTML = bookName;
+  var element = document.getElementById("book");
+  element.appendChild(div);
+
+}
+
+
+myLibrary.forEach(generateCard);
+
+//generateCard(myLibrary[0]);
+//generateCard(myLibrary[1]);
+
+document.getElementById("myBtn").addEventListener("click", function() {
+  alert("hello world")
+});
